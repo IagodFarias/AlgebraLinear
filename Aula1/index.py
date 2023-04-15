3a2c9b78d42992bf28cb57fc93e299a4e4aab47d
Danyelfarias <engenhariagofarias0803@gmail.com>
2023-04-14 21:01:47 -0300

    Update index.py

--- Aula1/index.py
+++ Aula1/index.py
@@ -1,10 +1,12 @@
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
 
 x = np.array(np.linspace(-100,100,50))
+print(x)
 y = x**2
-plt.
 
 
+#plt.plot(x,y)
+#plt.show()
 
 
